6632809f9aa3cc416251e3ea93e8c2d181539291
Rahul Kumar <71453998+rahulml40@users.noreply.github.com>
2020-09-21 00:39:21 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app=FastAPI()
 @app.get("/")
 async def  root():
     return ("this is my first Fast API Get")
+
+@app.get("/items/{item_id}")
+async def queryParam(item_id):
+    return {"Item ID is ": item_id}
